48d076a8c9560a9f4316fd4f1eb1be5374b0abab
Ben Bullock <benkasminbullock@gmail.com>
2017-08-24 09:22:20 +0900

    Update build script to make distro/git commit

--- build.pl
+++ build.pl
@@ -2,5 +2,23 @@
 use warnings;
 use strict;
 use FindBin '$Bin';
+use Getopt::Long;
 chdir $Bin or die $!;
-system ("perl Makefile.PL;make;make test") == 0 or die;
+my $ok = GetOptions (
+    verbose => \my $verbose,
+    dist => \my $dist,
+    all => \my $all,
+);
+if ($all) {
+    if (-f "$Bin/Makefile") {
+	system ("make clean") == 0 or warn "Make clean failed";
+    }
+    system ("git add .;git commit -a") == 0 or die "Git commit failed";
+}
+else {
+    system ("perl Makefile.PL;make;make test") == 0 or die;
+    if ($dist) {
+	system ("make manifest;make dist") == 0 or die;
+    }
+}
+
